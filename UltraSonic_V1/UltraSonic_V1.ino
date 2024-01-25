cd0df5f25c6e3a4218d408cb1a86ce9f9c6c1bc1
Saffi Haider <haides23@mcmaster.ca>
2024-01-25 01:01:25 -0500

    Removed LED support, updated pins, overloaded getDistance with getDistanceAvg

--- UltraSonic_V1/UltraSonic_V1.ino
+++ UltraSonic_V1/UltraSonic_V1.ino
@@ -1,14 +1,17 @@
-// defines pins numbers
-const int trigPin1 = 2;
-const int echoPin1 = 4;
+// Ultrasonic pins
+const int trigPin1 = 15;
+const int echoPin1 = 2;
 const int trigPin2 = 16;
-const int echoPin2 = 17;
-const int middleLED = 5;
-const int leftLED = 18;
-const int rightLED = 6;
-const int threshold = 100;
+const int echoPin2 = 4;
 
-// defines variables
+const int threshold = 50; // Minimum distance for obstacle to be registered
+const int delaytime = 1000; // Delay between each pulse 
+const int iterations = 3; // Averaging factor for each distance measurement
+
+//const int middleLED = 5;
+//const int leftLED = 18;
+//const int rightLED = 19;
+
 int distance1, distance2;
 
 void setup() {
@@ -17,72 +20,76 @@ void setup() {
   pinMode(trigPin2, OUTPUT); 
   pinMode(echoPin2, INPUT); 
   
-  pinMode(middleLED, OUTPUT);
-  pinMode(leftLED, OUTPUT);
-  pinMode(rightLED, OUTPUT);
+  //pinMode(middleLED, OUTPUT);
+  //pinMode(leftLED, OUTPUT);
+  //pinMode(rightLED, OUTPUT);
+  
   Serial.begin(9600); // Starts the serial communication
 }
 
 void loop() {
-  //distance1 = getDistance(trigPin1, echoPin1, 5);
-  distance1 = getDistanceAvg(trigPin1, echoPin1, 5);
-  // Prints the distance on the Serial Monitor
-  Serial.print("Distance 1: ");
-  Serial.println(distance1);
-  delayMicroseconds(10);
-  
+  // Measure distances
+
+  distance1 = getDistance(trigPin1, echoPin1, iterations);
+  //distance1 = getDistance(trigPin1, echoPin1);
+  delayMicroseconds(delaytime);
+  distance2 = getDistance(trigPin2, echoPin2, iterations);
   //distance2 = getDistance(trigPin2, echoPin2);
-  distance2 = getDistanceAvg(trigPin2, echoPin2, 5);
-  Serial.print("Distance 2: ");
-  Serial.println(distance2);
-  delayMicroseconds(10);
-  //delay(500);
-  
+  delayMicroseconds(delaytime);
+
+  // Compare distance to threshold
   bool middle = (distance1 < threshold) && (distance2 < threshold);
   bool left = (distance1 < threshold);
   bool right = (distance2 < threshold);
-  
+
+  // Trigger respective haptic motor
   if (middle) {
-    LEDControl(middleLED, true);
-    LEDControl(leftLED, false);
-    LEDControl(rightLED, false);  
+    Serial.println("middle");
+//    LEDControl(middleLED, true);
+//    LEDControl(leftLED, false);
+//    LEDControl(rightLED, false);  
   }
   
   else if (left) {
-    LEDControl(middleLED, false);
-    LEDControl(leftLED, true);  
-    LEDControl(rightLED, false);
+    Serial.println("left");
+//    LEDControl(middleLED, false);
+//    LEDControl(leftLED, true);  
+//    LEDControl(rightLED, false);
   }
   
   else if (right) {
-    LEDControl(middleLED, false);
-    LEDControl(leftLED, false);  
-    LEDControl(rightLED, true);
+    Serial.println("right");
+//    LEDControl(middleLED, false);
+//    LEDControl(leftLED, false);  
+//    LEDControl(rightLED, true);
   }
   
   else {
-    LEDControl(middleLED, false);
-    LEDControl(leftLED, false);  
-    LEDControl(rightLED, false);
+    Serial.println("N/A");
+//    LEDControl(middleLED, false);
+//    LEDControl(leftLED, false);  
+//    LEDControl(rightLED, false);
   }
 }
 
-int getDistance(int trigPin, int echoPin) {
+// Triggers ultrasonic sensor, measures pulse width, and returns distance in cm
+float getDistance(int trigPin, int echoPin) {
   // Clear trigPin
   digitalWrite(trigPin, LOW);
-  delayMicroseconds(2);
+  delayMicroseconds(10);
   
-  // Output ultraSonic wave
+  // Output UltraSonic wave
   digitalWrite(trigPin, HIGH);
   delayMicroseconds(10);
   digitalWrite(trigPin, LOW);
   
   // Scan for returned wave and calculate distance
   int duration = pulseIn(echoPin, HIGH);
-  return (duration * 0.034 / 2);
+  return (duration * 0.034 / 2.0);
 }
 
-float getDistanceAvg(int trigPin, int echoPin, int iterations) {
+// Averages the distance measurement
+float getDistance(int trigPin, int echoPin, int iterations) {
   float avgDistance = 0;
   for (int i = 0; i < iterations; i++) {
     avgDistance += getDistance(trigPin, echoPin);
@@ -90,11 +97,11 @@ float getDistanceAvg(int trigPin, int echoPin, int iterations) {
   return avgDistance/iterations;
 }
 
-void LEDControl(int LEDPin, bool condition) {
-  if (condition) {
-    digitalWrite(LEDPin, HIGH);
-  }
-  else{
-    digitalWrite(LEDPin, LOW);
-  }
-}
+//void LEDControl(int LEDPin, bool condition) {
+//  if (condition) {
+//    digitalWrite(LEDPin, HIGH);
+//  }
+//  else{
+//    digitalWrite(LEDPin, LOW);
+//  }
+//}
