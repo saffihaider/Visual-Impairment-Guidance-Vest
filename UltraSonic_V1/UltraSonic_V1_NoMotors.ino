c19fa4b1265ededbfd0c18fffe81b831b90aff48
Saffi Haider <haides23@mcmaster.ca>
2024-02-11 14:18:31 -0500

    Renamed files and changed ultrasonic pins

--- UltraSonic_V1/UltraSonic_V1_NoMotors.ino
+++ UltraSonic_V1/UltraSonic_V1_NoMotors.ino
@@ -1,8 +1,3 @@
-//Haptic Motor Controls
-#include <Wire.h>
-#include "Adafruit_DRV2605.h"
-Adafruit_DRV2605 drv1;
-
 // Ultrasonic pins
 const int trigPin = 15; // Pin used to trigger ranging
 const int echoPin1 = 5; // Ultrasonic 1 output 
@@ -13,27 +8,11 @@ const int delaytime = 500; // Delay between measurements
 const int iterations = 1; // Averaging factor (number of measurements to be averaged per cycle)
 int distance1, distance2;
 
-//Haptic Pins
-#define PWM_PIN_1 18 // Pin motor 2
-#define PWM_PIN_2 12 // Pin motor 3
-#define PWM_PIN_3 14 // Pin motor 4
-
 void setup() {
   pinMode(trigPin, OUTPUT); 
   pinMode(echoPin1, INPUT); 
   pinMode(echoPin2, INPUT);  
   Serial.begin(9600); // Starts the serial communication
-  
-  if (!drv1.begin()) {
-      Serial.println("Failed to find motors ");
-      while (1);
-    }
-
-  pinMode(PWM_PIN_1, OUTPUT); 
-  pinMode(PWM_PIN_2, OUTPUT);
-  pinMode(PWM_PIN_3, OUTPUT);
-
-  Serial.println("Haptic initialized");
 }
 
 void loop() {
@@ -48,40 +27,32 @@ void loop() {
   delayMicroseconds(970);
   
   // Scan for returned wave and measure pulse widths to calculate distance
-  distance1 = pulseIn(echoPin1, HIGH) * 0.034 / 2.0;
-  distance2 = pulseIn(echoPin2, HIGH) * 0.034 / 2.0;
+  distance1 = pulseIn(echoPin1, HIGH)* 0.034 / 2.0;
+  distance2 = pulseIn(echoPin2, HIGH)* 0.034 / 2.0;
   Serial.println(distance1);
   Serial.println(distance2);
 
   // Compare distance to threshold
+  bool middle = (distance1 < threshold) && (distance2 < threshold);
   bool left = (distance1 < threshold);
   bool right = (distance2 < threshold);
-  bool middle = left && right;
-  
+
   // Trigger respective haptic motor
   if (middle) {
-    drv1.setWaveform(8, 14);
-    drv1.go();
-    analogWrite(PWM_PIN_1, 255); // Set PWM duty cycle
-    analogWrite(PWM_PIN_2, 255); // 255 for max vibration
-    analogWrite(PWM_PIN_3, 255);
-    delay(10);
+    Serial.println("middle");
+    // Add haptic controls here
   }
+ 
   else if (left) {
     Serial.println("left");
-    drv1.setWaveform(8, 14);
-    drv1.go();
-    analogWrite(PWM_PIN_1, 255); // Set PWM duty cycle
-    delay(10);
-
+    // Add haptic controls here
   }
+  
   else if (right) {
     Serial.println("right");
-    
-    analogWrite(PWM_PIN_2, 255); // 255 for max vibration
-    analogWrite(PWM_PIN_3, 255); 
-    delay(10); 
+    // Add haptic controls here
   }
+  
   else {
     Serial.println("N/A");
     // Add haptic controls here
