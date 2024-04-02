8c89dceb05f4061dfb0fbb1050d1f2dfe10a90e8
Saffi Haider <haides23@mcmaster.ca>
2024-04-02 00:58:53 -0400

    Renamed files, updated haptic driver commands

--- UltraSonic_V1/UltraSonic_V1.ino
+++ UltraSonic_V1/UltraSonic_V1.ino
@@ -1,55 +1,60 @@
-//Haptic Motor Controls
-#include <Wire.h>
-#include "Adafruit_DRV2605.h"
-Adafruit_DRV2605 drv1;
 
 // Ultrasonic pins
 const int trigPin = 15; // Pin used to trigger ranging
 const int echoPin1 = 5; // Ultrasonic 1 output 
 const int echoPin2 = 4; // Ultrasonic 2 output 
 
-const int threshold = 50; // Minimum distance for obstacle to be registered
-const int delaytime = 500; // Delay between measurements
-const int iterations = 1; // Averaging factor (number of measurements to be averaged per cycle)
+const int threshold = 50; // Minimum distance for obstacle to be registered (in cm)
+const int iterations = 5;
 int distance1, distance2;
 
 //Haptic Pins
-#define PWM_PIN_1 18 // Pin motor 2
-#define PWM_PIN_2 12 // Pin motor 3
-#define PWM_PIN_3 14 // Pin motor 4
+#define PWM_PIN_0 22 // Pin motor 1: Left
+#define PWM_PIN_1 25 // Pin motor 2: Left  
+#define PWM_PIN_2 12 // Pin motor 3: Right
+#define PWM_PIN_3 18 // Pin motor 4: Right
+
+//LED Pins
+const int leftLED = 23;   
+const int rightLED = 14;  
+const int middleLED = 21;  
 
 void setup() {
-  pinMode(trigPin, OUTPUT); 
+  pinMode(trigPin, OUTPUT);  
   pinMode(echoPin1, INPUT); 
   pinMode(echoPin2, INPUT);  
   Serial.begin(9600); // Starts the serial communication
   
-  if (!drv1.begin()) {
-      Serial.println("Failed to find motors ");
-      while (1);
-    }
-
+  pinMode(PWM_PIN_0, OUTPUT); 
   pinMode(PWM_PIN_1, OUTPUT); 
   pinMode(PWM_PIN_2, OUTPUT);
   pinMode(PWM_PIN_3, OUTPUT);
+  pinMode(leftLED, OUTPUT); 
+  pinMode(rightLED, OUTPUT); 
+  pinMode(middleLED, OUTPUT);
 
-  Serial.println("Haptic initialized");
 }
 
-void loop() {
-  // Clear trigPin
-  digitalWrite(trigPin, LOW);
-  delayMicroseconds(10);
+void loop() {  
+  distance1 = 0;
+  distance2 = 0;
+  for (int i = 0; i < iterations; i++)
+  {
+    // Output UltraSonic wave
+    digitalWrite(trigPin, HIGH);
+    
+    // Scan for returned wave and measure pulse widths to calculate distance
+    distance1 += pulseIn(echoPin1, HIGH) * 0.034 / 2.0;
+    distance2 += pulseIn(echoPin2, HIGH) * 0.034 / 2.0;
   
-  // Output UltraSonic wave
-  digitalWrite(trigPin, HIGH);
-  delayMicroseconds(30);
-  digitalWrite(trigPin, LOW);
-  delayMicroseconds(970);
-  
-  // Scan for returned wave and measure pulse widths to calculate distance
-  distance1 = pulseIn(echoPin1, HIGH) * 0.034 / 2.0;
-  distance2 = pulseIn(echoPin2, HIGH) * 0.034 / 2.0;
+    // Stop outputting UltraSonic wave
+    digitalWrite(trigPin, LOW);
+  }
+  // Take average distance
+  distance1 /= iterations;
+  distance2 /= iterations;
+
+  // Print
   Serial.println(distance1);
   Serial.println(distance2);
 
@@ -58,32 +63,50 @@ void loop() {
   bool right = (distance2 < threshold);
   bool middle = left && right;
   
-  // Trigger respective haptic motor
+  // Clear haptic PWM signals
+  digitalWrite(PWM_PIN_0, LOW);
+  digitalWrite(PWM_PIN_1, LOW);
+  digitalWrite(PWM_PIN_2, LOW);
+  digitalWrite(PWM_PIN_3, LOW);
+  digitalWrite(leftLED, LOW);
+  digitalWrite(rightLED, LOW);
+  digitalWrite(middleLED, LOW);
+  
+  // Trigger respective haptic motor and LED
   if (middle) {
-    drv1.setWaveform(8, 14);
-    drv1.go();
-    analogWrite(PWM_PIN_1, 255); // Set PWM duty cycle
-    analogWrite(PWM_PIN_2, 255); // 255 for max vibration
+    Serial.println("middle");
+    analogWrite(PWM_PIN_0, 255);
+    analogWrite(PWM_PIN_1, 255);
+    analogWrite(PWM_PIN_2, 255);
     analogWrite(PWM_PIN_3, 255);
-    delay(10);
+    digitalWrite(middleLED, HIGH);
   }
   else if (left) {
     Serial.println("left");
-    drv1.setWaveform(8, 14);
-    drv1.go();
-    analogWrite(PWM_PIN_1, 255); // Set PWM duty cycle
-    delay(10);
-
+    analogWrite(PWM_PIN_0, 255);
+    analogWrite(PWM_PIN_1, 255);
+    analogWrite(PWM_PIN_2, 0); 
+    analogWrite(PWM_PIN_3, 0); 
+    digitalWrite(leftLED, HIGH);
   }
   else if (right) {
     Serial.println("right");
-    
-    analogWrite(PWM_PIN_2, 255); // 255 for max vibration
-    analogWrite(PWM_PIN_3, 255); 
-    delay(10); 
+    analogWrite(PWM_PIN_0, 0); 
+    analogWrite(PWM_PIN_1, 0); 
+    analogWrite(PWM_PIN_2, 255);
+    analogWrite(PWM_PIN_3, 255);
+    digitalWrite(rightLED, HIGH);
   }
   else {
-    Serial.println("N/A");
-    // Add haptic controls here
+    Serial.println("No obstacle");
+    analogWrite(PWM_PIN_0, 0); 
+    analogWrite(PWM_PIN_1, 0); 
+    analogWrite(PWM_PIN_2, 0);
+    analogWrite(PWM_PIN_3, 0);
+    digitalWrite(leftLED, LOW);
+    digitalWrite(rightLED, LOW);
+    digitalWrite(middleLED, LOW);
   }
+  
+  //delay(delaytime); // Delay for stability
 }
